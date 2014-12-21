13fe609e837aaef248848b1276a02132f1d128f9
Howard Fackrell <howard.fackrell@octanner.com>
2014-12-21 01:09:41 -0700

    Add preview rotations

--- js/app.js
+++ js/app.js
@@ -1,7 +1,32 @@
 
+var rotate = true;
+
 $(document).ready(function() {
   $(".preview-well").on('click', function() {
   	var url = $(this).find("img").attr("href");
   	window.open(url, '_blank');
   });
+
+  $(".preview-well").find('p').slideUp();
+
+  $(".preview-well").on('mouseenter',  function() {
+    $(this).find('p').slideDown();	
+    rotate = false;
+  });
+
+  $(".preview-well").on('mouseleave', function() {
+    $(this).find('p').slideUp();	
+    rotate = true;
+  });
 });
+
+
+window.setInterval(function() {
+	if (rotate) {
+	$(".preview").first().slideUp(400, function() {
+		var preview = $(".preview").first().detach();
+		$(".previews").append(preview);	
+		preview.slideDown();
+	});
+	}
+}, 10000);
